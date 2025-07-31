0d84fd336e339d370b4cc89784faf1d3832e1035
Salman Hossain <salmanhossain500@gmail.com>
2025-07-31 12:48:18 -0400

    fix: update content security policy

--- next.config.js
+++ next.config.js
@@ -24,6 +24,12 @@ const nextConfig = {
             value: '1; mode=block'
           },
           {
+            key: 'Content-Security-Policy',
+            value: process.env.NODE_ENV === 'production'
+              ? "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https: https://adept-cow-924.convex.cloud; frame-ancestors 'none';"
+              : "default-src 'self' 'unsafe-inline' 'unsafe-eval'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https: ws: wss: https://adept-cow-924.convex.cloud; frame-ancestors 'none';"
+          },
+          {
             key: 'Permissions-Policy',
             value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
           }
